48e549ec89fc1d27f9a61112c89adaa9f9e2a724
n0n4w3-jj <story.botsfordli@gmail.com>
2026-01-27 11:26:00 +0800

    helix -> evil-helix

--- home-manager/modules/helix.nix
+++ home-manager/modules/helix.nix
@@ -1,7 +1,19 @@
+{ pkgs, ... }:
 {
   programs.helix = {
     enable = true;
+    package = pkgs.evil-helix;
     settings = {
+      keys.normal = {
+        g = { e = "move_prev_word_end"; };
+        "A-s" = ":toggle-option soft-wrap.enable";
+        "A-a" = ":sh alacritty msg create-window --working-directory \"$(pwd)\"";
+      };
+      keys.select = {
+        g = { e = "move_prev_word_end"; };
+        "A-s" = ":toggle-option soft-wrap.enable";
+        "A-a" = ":sh alacritty msg create-window --working-directory \"$(pwd)\"";
+      };
       editor = {
         line-number = "relative";
         auto-format = false;
@@ -23,6 +35,7 @@
       };
       editor.indent-guides = {
         render = true;
+        rainbow-option = "dim";
       };
       editor.smart-tab = {
         enable = false;
